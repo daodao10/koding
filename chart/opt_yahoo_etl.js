6b93dc5181f1c3a3b17d7b29a8843b9ebfca10de
daodao10 <bebest10@gmail.com>
2014-12-09 01:38:46 +0800

    update gitignore

--- chart/opt_yahoo_etl.js
+++ chart/opt_yahoo_etl.js
@@ -1,8 +1,14 @@
 var fs = require("fs"),
-    util = require('util');
+    util = require('util'),
+    myUtil = require('../nd/MyUtil'),
+    etlUtil = require('./EtlUtil');
 
 var settings = {
+    DestFolder: "../../daodao10.github.io/chart/",
     ItemFormat: "[new Date(%s, %s - 1, %s), %d, '%d']",
+    HasHeader: true,
+    HasLastBlank: true,
+
     "Y": {
         IsCompactDate: false,
         xCell: 0,
@@ -11,32 +17,89 @@ var settings = {
     "SI": {
         IsCompactDate: true,
         xCell: 3,
-        yCell: 8
+        yCell: 7
+    },
+    "SQ": {
+        IsCompactDate: false,
+        xCell: 0,
+        yCell: 4
     }
 };
 
-
-var dataFile = "history_GentingSing_20141111.csv",
-    folder = "../../daodao10.github.io/chart/d/",
-    outputFile = folder + "gp_sg.js",
-    source = "SI";
+batchProcess('yahoo.txt');
+// batchProcess();
 
 
-generate(dataFile, outputFile);
+function batchProcess(filename) {
+    var index = 0,
+        period,
+        cells,
+        srcFile,
+        destFile;
 
-function generate(filepath, newFile) {
-    fs.readFile(filepath, function(err, data) {
+    if (filename) {
+        myUtil.readlines(filename, function(row) {
+            index++;
+
+            if (index === 1) {
+                var matches = /\[(\w+)\s*:\s*(\w+)\s*:\s*(\w+),\s*(\w+)\]/gi.exec(row);
+                settings["source"] = etlUtil.encode_source(matches[1]);
+                settings["market"] = matches[2];
+                settings["period"] = [matches[3], matches[4]];
+
+                return;
+            }
+
+            for (var i in settings.period) {
+                period = etlUtil.encode_period(settings.period[i]);
+                cells = row.split(',');
+                srcFile = "{0}_{1}_{2}.csv".format(settings.market, cells[1], period);
+                destFile = "{3}{0}/{1}_{2}.js".format(settings.market, cells[1], period, settings.DestFolder);
+                // console.log(srcFile, destFile);
+                generate(srcFile, destFile);
+            };
+
+            // console.log("finished:", index.toString());
+        });
+    } else {
+        // single src file
+
+        var code = '^N225';
+        settings["source"] = etlUtil.encode_source('stooq');
+        settings["period"] = etlUtil.encode_period('daily');
+        settings["market"] = 'world';
+
+        srcFile = "{0}_{1}_{2}.csv".format(settings.market, code, settings.period);
+        destFile = "{3}{0}/{1}_{2}.js".format(settings.market, code, settings.period, settings.DestFolder);
+        generate(srcFile, destFile);
+
+    }
+}
+
+function generate(srcFile, output) {
+    fs.readFile(srcFile, function(err, data) {
         if (err) {
             console.log(err);
         } else {
             var array = data.toString().split("\n");
 
-            array.shift();
+            if (settings.HasHeader) { // remove header
+                array.shift();
+            }
+            if (settings.HasLastBlank) { // remove last blank line
+                array.pop();
+            }
+            // sorting by date: newest to oldest
+            if (settings.source === "SQ") {
+                array.reverse();
+            };
+
             var data = array.map(mapFunc);
 
-            var content = "var data=[" + data.join(',') + "];";
+            var content = "var data=[" + data.join(',') + "];\
+                var source='" + etlUtil.decode_source(settings.source) + "';";
 
-            fs.writeFile(newFile, content, function(err) {
+            fs.writeFile(output, content, function(err) {
                 if (err) throw err;
                 console.log("Saved!");
             });
@@ -46,11 +109,11 @@ function generate(filepath, newFile) {
 
 function mapFunc(line) {
     var part = line.split(",");
-    var yCell = part[settings[source].yCell];
-    value = Math.log(yCell);
-    if (isNaN(value) || value < -0) return null;
-    var xCell = part[settings[source].xCell];
-    return settings[source].IsCompactDate ?
+    var yCell = part[settings[settings.source].yCell];
+    value = Math.log(yCell) / Math.LN2;
+    if (isNaN(value)) return null; // || value < -0
+    var xCell = part[settings[settings.source].xCell];
+    return settings[settings.source].IsCompactDate ?
         util.format(settings.ItemFormat, xCell.substring(0, 4), xCell.substring(4, 6), xCell.substring(6, 8), value, yCell) :
         util.format(settings.ItemFormat, xCell.substring(0, 4), xCell.substring(5, 7), xCell.substring(8, 10), value, yCell);
 }
