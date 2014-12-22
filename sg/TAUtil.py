71224f21d5f025e34244421c903c9fbddbca5c74
daodao10 <bebest10@gmail.com>
2014-12-22 18:11:57 +0800

    in progress

--- sg/TAUtil.py
+++ sg/TAUtil.py
@@ -8,19 +8,37 @@ from koding import web_tools
 from SymbolUtil import SymbolUtil
 from ShareInvestor import ShareInvestor
 
-class TAUtil():
+class TAUtil:
     @staticmethod
     def download(counter, cookies):
         url = ShareInvestor.Urls["price-download"] % counter
         filename = counter.replace('.SI','') + ".csv"
         return web_tools.download(url, headers=cookies)
-        
+
+
 if __name__ == "__main__":
 
-    cookies = cookies = ShareInvestor.getPersistentCookie()
+    # cookies = ShareInvestor().refreshCookie()
+    cookies = ShareInvestor.getPersistentCookie()
     # print cookies
 
-    # # signle counter testing
-    # #
-    counter = "P8A.SI"
-    print TAUtil.download(counter, cookies)
+    # signle counter testing
+    #
+    counter = "Q1P.SI"
+    zipFileName = TAUtil.download(counter, cookies)
+    
+    if zipFileName:
+        import zipfile
+        fileHandle = open(zipFileName, 'rb')
+
+        zf = zipfile.ZipFile(fileHandle)
+        for name in zf.namelist():
+            try:
+                data = zf.read(name)
+            except KeyError:
+                print 'ERROR: did not find %s in zip file' % name
+            else:
+                print name, ':'
+                print data
+
+        fileHandle.close() 
