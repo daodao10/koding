121d15095aefe46234d4295cae0c55ca3aeddd84
daodao10 <bebest10@gmail.com>
2014-12-23 15:01:51 +0800

    separate asia into sg & cn

--- asia.py
+++ asia.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python
-import os
-import platform
-
+import sys
 import sgx, sse, web_tools
 
-today = None
-# today = web_tools.today(utcDiff = +4)
+def main():
+    today = None
+    # today = web_tools.today(utcDiff = +4)
+    dbUri = web_tools.getDbUri()
 
-dbUri = web_tools.getDbUri()
+    args = sys.argv
+    if len(args) == 2:
+        if args[1] == 'sg':
+            process4SG(dbUri, today)
+        elif args[1] == 'cn':
+            process4CN(dbUri, today)
+        else:
+            process4SG(dbUri, today)
+            process4CN(dbUri, today)
+    else:
+        print 'USAGE: python asia.py [cn|sg]'
 
-s = sgx.SGX(dbUri = dbUri, today = today)
-s.getSummary()
-s.getDayTick()
+def process4SG(dbUri, today):
+    print 'processing for SG'
+    s = sgx.SGX(dbUri = dbUri, today = today)
+    s.getSummary()
+    s.getDayTick()
 
-s = sse.SSE(dbUri = dbUri, today = today)
-s.getDayTick()
+def process4CN(dbUri, today):
+    print 'processing for China'
+    s = sse.SSE(dbUri = dbUri, today = today)
+    s.getDayTick()
+
+
+if __name__ == '__main__':
+    main()
