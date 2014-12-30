569e7adb3ebaeb17399bf56ccee4ff3ccda84066
daodao10 <bebest10@gmail.com>
2014-12-30 17:36:08 +0800

    accept args from command line

--- learning/capture/out.sh
+++ learning/capture/out.sh
@@ -6,19 +6,25 @@ if [ ! -d "$today" ]; then
     mkdir $today
 fi
 
-read -p "enter market(cn|us): " market
+if [ "$#" -gt 0 ]
+then
+    market=$1
+else
+    read -p "enter market(cn|us): " market
+fi
+
 case $market in
 us)
-	echo 'capture from finviz.com'
-	phantomjs finviz.js $today
-	;;
+    echo 'capture from finviz.com ...'
+    phantomjs finviz.js $today
+    ;;
 cn)
-	echo 'capture from eastmoney.com'
-	for f in $(cat ./note.txt)
-	do
-	    phantomjs ct.js $f $today
-	done
-	;;
+    echo 'capture from eastmoney.com ...'
+    for f in $(cat ./note.txt)
+    do
+        phantomjs ct.js $f $today
+    done
+    ;;
 *)
-	echo "don't support yet";;
+    echo "don't support yet";;
 esac
