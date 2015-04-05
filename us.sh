74623ceebdb313972fe1d55f1d017750d115d0c7
daodao10 <bebest10@gmail.com>
2015-04-06 00:45:19 +0800

    add stockcharts etl to us.sh

--- us.sh
+++ us.sh
@@ -2,6 +2,7 @@
 # delay 15 hours based on UTC+8
 today=`date -u -v-7H +%Y%m%d` # mac
 # today=`date -u -d '-7 Hour' +%Y%m%d`
+# today='20150402'
 
 echo 'get data from gurufocus.com ...'
 python gurufocus.py
@@ -19,6 +20,10 @@ echo 'summary NH-NL data'
 node nhnl_us_sum_etl.js $today
 echo 'done'
 
+cd ../nd
+
+echo 'get data from stockcharts ...'
+node sc_etl.js
 
 cd ../learning/capture
 
