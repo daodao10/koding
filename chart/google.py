c5f1b5cecbe52e8aa8a34207fac4acd1da6e8b6a
daodao10 <bebest10@gmail.com>
2015-04-15 15:59:18 +0800

    refactor data etl of google

--- chart/google.py
+++ chart/google.py
@@ -40,11 +40,11 @@ class Quote(object):
   def append(self,dt,open_,high,low,close,volume):
     self.date.append(dt.date())
     self.time.append(dt.time())
-    self.open_.append(float(open_))
-    self.high.append(float(high))
-    self.low.append(float(low))
-    self.close.append(float(close))
-    self.volume.append(int(volume))
+    self.open_.append(self.convert_2_float(open_))
+    self.high.append(self.convert_2_float(high))
+    self.low.append(self.convert_2_float(low))
+    self.close.append(self.convert_2_float(close))
+    self.volume.append(self.convert_2_int(volume))
       
   def to_csv(self):
     return 'Symbol,Date,Time,Open,High,Low,Close,Volume\n' + ''.join(["{0},{1},{2},{3:.2f},{4:.2f},{5:.2f},{6:.2f},{7}\n".format(self.symbol,
@@ -59,15 +59,26 @@ class Quote(object):
   def read_csv(self,filename):
     self.symbol = ''
     self.date,self.time,self.open_,self.high,self.low,self.close,self.volume = ([] for _ in range(7))
+    i = 0
     for line in open(filename,'r'):
+      i += 1
+      if i == 1:
+        continue
       symbol,ds,ts,open_,high,low,close,volume = line.rstrip().split(',')
       self.symbol = symbol
-      dt = datetime.datetime.strptime(ds+' '+ts,self.DATE_FMT+' '+self.TIME_FMT)
-      self.append(dt,open_,high,low,close,volume)
+      self.append(datetime.datetime.strptime(ds+' '+ts, self.DATE_FMT+' '+self.TIME_FMT),open_,high,low,close,volume)
     return True
 
   def patch(self, url, start, total):
     pass
+
+  def convert_2_float(self, str):
+    return 0 if str == "-" else float(str.replace(',', ''))
+    # float(str.replace(',', ''))
+
+  def convert_2_int(self, str):
+    return 0 if str == "-" else int(str.replace(',', ''))
+    # int(str.replace(',', ''))
 
   def __repr__(self):
     return self.to_csv()
@@ -97,9 +108,7 @@ class GoogleQuote(Quote):
       #csv.reverse()
       for bar in xrange(1,len(csv)-1):
         ds,open_,high,low,close,volume = csv[bar].rstrip().split(',')
-        open_,high,low,close = [float(x) for x in [open_,high,low,close]]
-        dt = datetime.datetime.strptime(ds,'%d-%b-%y')
-        self.append(dt,open_,high,low,close,volume)
+        self.append(datetime.datetime.strptime(ds,'%d-%b-%y'),open_,high,low,close,volume)
     
   def patch(self, url, start, total):
     PageSize = 200
@@ -108,13 +117,19 @@ class GoogleQuote(Quote):
 
     # debug.save('data.txt', content)
     list = re.findall(self.RegRow, content)
+    # group: date, o, h, l, c, volume
     for group in list:
+      # decide non-trading day by close price
+      # if group[4] == '-':
       if group[1] == '-':
         print 'non-trading day: %s' % str(group)
         continue
       self.append(datetime.datetime.strptime(group[0],'%b %d, %Y'),
-        float(group[1].replace(',', '')), float(group[2].replace(',', '')), float(group[3].replace(',', '')), float(group[4].replace(',', '')),
-        0 if group[5] == "-" else group[5].replace(',', ''))
+        group[1],
+        group[2],
+        group[3],
+        group[4],
+        group[5])
 
     if start == 0:
       for m in self.RegPagination.finditer(content):
@@ -125,28 +140,99 @@ class GoogleQuote(Quote):
       self.patch(url, start + PageSize, total)
 
 
+import sys
 import package
 package.append_dir('../..')
 
 from koding import debug
 
+def smoke_test():
+  q = GoogleQuote('aapl','2015-01-01')              # download year to date Apple data
+  print q                                           # print it out
+  print '-------------------- get pass --------------------'
+  
+  q = GoogleQuote('orcl','2011-11-01','2011-11-30') # download Oracle data for February 2011
+  q.write_csv('orcl.csv')                           # save it to disk
+  print '-------------------- write_csv pass --------------------'
+  
+  q = Quote()                                       # create a generic quote object
+  q.read_csv('orcl.csv')                            # populate it with our previously saved data
+  print q                                           # print it out
+  print '-------------------- read_csv pass --------------------'
+
+def help():
+  print 'USAGE: python google.py <filename>'
+
+def parse_setting(str):
+  RegSetting = re.compile(r'\[(\w+)\s*:\s*(\w+)\s*:\s*(\w+)(,\s*(\w+))*\]')
+  m = RegSetting.search(str)
+  if m:
+    # print m.groups()
+    setting = {"source":m.group(1), "market":m.group(2)}
+    setting["period"] = [m.group(3)]
+
+    if m.group(4):
+      setting["period"].append(m.group(5))
+
+    return setting
+
+  return None
+
+def encode_period(period):
+  if period == "monthly":
+    return "m"
+  elif period == "weekly":
+    return "w"
+  else:
+    return "d"
+
+def get_output(settings, code):
+  return "%s_%s_%s.csv" % (settings["market"], code, encode_period(settings["period"][0]))
+
+def main():
+  '''
+  currently support market: world, us, hk, cn, sg
+  '''
+
+  filename = None
+  args = sys.argv
+  if len(args) == 2:
+      filename = args[1]
+  else:
+      help()
+
+  i = 0
+  settings = None
+  for line in open(filename,'r'):
+    i += 1
+    if i == 1:
+      settings = parse_setting(line)
+      print settings
+      continue
+
+    symbol,code = line.rstrip().split(',')
+    if not settings:
+      break
+
+    if settings["market"] == "cn":
+      if symbol.startswith("6"):
+        symbol = "SHA:" + symbol
+      else:
+        symbol = "SHE:" + symbol
+    elif settings["market"] == "hk":
+      symbol = "HKG:" + symbol
+    elif settings["market"] == "sg":
+      symbol = "SGX:" + symbol
+
+    q = GoogleQuote(symbol,'1985-01-01', '2015-12-31')
+    q.write_csv(get_output(settings, code))
+
+    print '-------------------- done %s --------------------' % symbol
+
+
 if __name__ == '__main__':
-
-  # q = GoogleQuote('aapl','2015-01-01')              # download year to date Apple data
-  # print q                                           # print it out
-  
-  # q = GoogleQuote('orcl','2011-11-01','2011-11-30') # download Oracle data for February 2011
-  # q.write_csv('orcl.csv')                           # save it to disk
-  
-  # q = Quote()                                       # create a generic quote object
-  # q.read_csv('orcl.csv')                            # populate it with our previously saved data
-  # print q                                           # print it out
-
-  # q = GoogleQuote('^HXC','1985-01-01', '2015-12-31')
-  # q.write_csv('world_^HXC_d.csv')
-
-  q = GoogleQuote('HKG:83136','1985-01-01', '2015-12-31')
-  q.write_csv('hk_83136_d.csv')
-
-  # q = GoogleQuote('601328','1985-01-01', '2015-12-31')
-  # q.write_csv('cn_601328_d.csv')
+  forTest = True
+  if forTest:
+    smoke_test()
+  else:
+    main()
