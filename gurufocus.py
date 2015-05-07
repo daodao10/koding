8142d3c3d5fe3e7e727298f31f11655210f6105d
daodao10 <bebest10@gmail.com>
2015-05-07 21:57:07 +0800

    update extractor of marketvalue

--- gurufocus.py
+++ gurufocus.py
@@ -6,7 +6,8 @@ from mymongo import MyMongo
 
 
 class GuruFocus:
-    __regData = re.compile(r"\{\s*animation:false,\s*name\s*:\s*'TMC\/GDP',\s*data\s*:(.+?\]\])")
+    __ChartDataFormat = r"\{\s*animation:false,\s*name\s*:\s*'%s',\s*data\s*:(.+?\]\])"
+    __ChartNames = { "GDP": "GDP", "TMC": "Wilshire Total Market", "TMC2GDP": "TMC\/GDP" }
     # __regTable = re.compile(r'<table class="at">(.*)</table>', re.S)
     __regTable = re.compile(r'Where are we today \((.*)\).* <td>(.*)</td>')
     __regToday = re.compile(r'<b>(\d{2}/\d{2}/\d{4})</b>')
@@ -23,8 +24,8 @@ class GuruFocus:
         else:
             return None
 
-    def parseData(self, content):
-        m = self.__regData.search(content)
+    def parseData(self, content, regex):
+        m = regex.search(content)
         if m:
             content = m.group(1)
             arr = eval(content)
@@ -53,7 +54,8 @@ class GuruFocus:
             return {"date": web_tools.strDate(today), "desc": desc, "table": table, "market": "US"}
 
     def saveTmc2GdpData(self, content, refresh = True):
-        arr = self.parseData(content)
+        regex = re.compile(self.__ChartDataFormat % self.__ChartNames["TMC2GDP"])
+        arr = self.parseData(content, regex)
         r = []
         for x in arr:
             r.append({"date": web_tools.strLocalDate(web_tools.getDateFromTimestamp(x[0]), utcDiff = -8), "value": x[1], "market": "US"})
@@ -73,19 +75,58 @@ class GuruFocus:
             collection = self.__dbContext.collection("tmc2gdp_day")
             collection.insert(data)
 
+    def extractChartData(self, content, chartKey, refresh):
+        '''
+        refresh means get the latest one
+        '''
+        r = {}
+        regex = re.compile(self.__ChartDataFormat % self.__ChartNames[chartKey])
+        arr = self.parseData(content, regex)
+        if refresh:
+            x = arr[-1]
+            r[web_tools.strLocalDate(web_tools.getDateFromTimestamp(x[0]), utcDiff = -8)] = x[1]
+        else:
+            for x in arr:
+                r[web_tools.strLocalDate(web_tools.getDateFromTimestamp(x[0]), utcDiff = -8)] = x[1]
+
+        return r
+
+    def saveMarketValue(self, content, refresh = True):
+        merged = []
+        rows = []
+        keys = self.__ChartNames.keys()
+
+        for i in range(0, len(keys)): 
+            rows.append(self.extractChartData(content, keys[i], refresh))
+
+        for k in rows[0].keys():
+            item = {"date": k}
+            for x in range(0,len(keys)):
+                item[keys[x]] = rows[x][k]
+            merged.append(item)
+
+        # sort
+        merged.sort(key = lambda i: i["date"])
+
+        collection = self.__dbContext.collection("MV_us")
+        collection.insert(merged)
+
     def doWork(self):
         # get market valuations
         url = "http://www.gurufocus.com/stock-market-valuations.php"
         content = self.fetch(url)
         if content:
             # web_tools.save('content.txt', content) 
-            self.saveTmc2GdpDaily(content)
 
-            # first time set refresh = False
-            self.saveTmc2GdpData(content, True)
+            # self.saveTmc2GdpDaily(content)
+
+            # # first time set refresh = False
+            # self.saveTmc2GdpData(content, True)
+
+            self.saveMarketValue(content)
         else:
             print "empty"
-            
+
 
 if __name__ == "__main__":
     dbUri = web_tools.getDbUri(key="QuotesDbUri")
