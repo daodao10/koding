4ef2d640287fad714c6356c82bfd7ad7255a5aa6
daodao10 <bebest10@gmail.com>
2015-05-27 01:42:13 +0800

    replace the data source google with ws right back data for cn

--- chart/opt_etl.js
+++ chart/opt_etl.js
@@ -91,13 +91,21 @@ function fx(newRows, index, total, counter) {
                 srcFile = "../{0}/dest/{1}.csv".format(settings.market, cells[0]);
             } else if (settings["source"] === "WS") {
                 if (cells[4] == 1) {
+                    // reject(new Error('ignore'));
+                    // return;
+                    if (cells[0].startsWith('SH')) {
+                        srcFile = "../../wsWDZ/etl-2/SH/{0}.txt".format(cells[0]);
+                    } else {
+                        srcFile = "../../wsWDZ/etl-2/SZ/{0}.txt".format(cells[0]);
+                    }
+                } else {
                     reject(new Error('ignore'));
                     return;
-                }
-                if (cells[0].startsWith('SH')) {
-                    srcFile = "../../wsWDZ/etl/SH/{0}.txt".format(cells[0]);
-                } else {
-                    srcFile = "../../wsWDZ/etl/SZ/{0}.txt".format(cells[0]);
+                    if (cells[0].startsWith('SH')) {
+                        srcFile = "../../wsWDZ/etl/SH/{0}.txt".format(cells[0]);
+                    } else {
+                        srcFile = "../../wsWDZ/etl/SZ/{0}.txt".format(cells[0]);
+                    }
                 }
             } else { // common
                 if (settings.market === "hk" && cells[6] == 1) {
