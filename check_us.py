aca6e5a00d002f7dca3ff45e45e91f3ae3b108bb
daodao10 <bebest10@gmail.com>
2015-05-31 18:28:47 +0800

    check us daily data

--- check_us.py
+++ check_us.py
@@ -4,6 +4,7 @@ from mymongo import MyMongo
 import web_tools
 
 if __name__ == "__main__":
+    # today = "20150528"
     today = web_tools.today(utcDiff=-15)
     query = {"date": today}
     dbUri = web_tools.getDbUri(key="QuotesDbUri")
@@ -21,5 +22,16 @@ if __name__ == "__main__":
 
     print "-------today's market value data-------"
     rows = client.collection("MV_us").find(query)
+    for r in rows:
+        print r
+
+    print "-------today's new-high/new-low data-------"
+    query = {"d": today}
+    rows = client.collection("nhnl").find(query)
+    for r in rows:
+        print r
+
+    query = {"_id": today}
+    rows = client.collection("nhnl_sum").find(query)
     for r in rows:
         print r
