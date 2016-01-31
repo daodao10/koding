4d7c3f55dccafffc2a630952037b439bd5f21dcd
daodao10 <bebest10@gmail.com>
2016-02-01 02:21:39 +0800

    add em-util

---
+++ nd/EM_Quote.js
@@ -0,0 +1,144 @@
+//http://hqdigi2.eastmoney.com/EM_Quote2010NumericApplication/Index.aspx?Type=F&jsName=zjlx_hq&id=6000371&rt=48473768
+
+var fs = require('fs'),
+    myUtil = require('./MyUtil'),
+    anounymous = require('./ProtoUtil');
+
+const Urls = {
+        'FA': '/EM_Finance2014NumericApplication/JS.aspx?type={reportType}&sty={subReportType}&st=(Code)&sr=1&p={page}&ps={pageSize}&js=var%20daoESData={pages:(pc),data:[(x)]}{param}',
+        'DC': '/EM_DataCenter/JS.aspx?type={reportType}&sty={subReportType}&st={sortType}&sr={sortRule}&p={page}&ps={pageSize}&js=var%20daoESData={pages:(pc),data:[(x)]}{param}'
+    },
+    debug = true;
+
+function _get(options) {
+    return new Promise(function(resolve, reject) {
+        myUtil.get(options, function(data, statusCode) {
+            if (debug) {
+                console.log(options.path);
+            }
+            if (statusCode !== 200) {
+                console.error('error occurred: ', statusCode);
+                reject({
+                    url: options.path,
+                    error: statusCode
+                });
+            }
+
+            resolve(data);
+        });
+    });
+}
+
+function _prepareUrl(options) {
+    var _param = options.param,
+        _sort = "1",
+        param = "";
+
+    for (var key in _param) {
+        param += ("&" + key + "=" + _param[key])
+    }
+
+    _url = Urls[options.dataType];
+
+    _url = _url.replace("{reportType}", options.reportType);
+    _url = _url.replace("{subReportType}", options.subReportType);
+    _url = _url.replace("{pageSize}", options.pagesize);
+    _url = _url.replace("{page}", options.page);
+    _url = _url.replace("{sortType}", options.sort.id);
+    _url = _url.replace("{sortRule}", options.sort.desc ? "-1" : "1");
+    _url = _url.replace("{param}", param);
+
+    return _url
+};
+
+function _save(csvFile, data) {
+    fs.writeFile(csvFile, data, function(err) {
+        if (err) {
+            throw err;
+        }
+        console.log('saved.');
+    });
+}
+
+
+function EM_Quote() {}
+
+EM_Quote.prototype.Path = function(options) {
+    this.path = _prepareUrl(options);
+};
+
+EM_Quote.prototype.Runner = function(options) {
+
+    var options = myUtil.extend({
+        //host:"",
+        //path: "",
+        //csvFile:"",
+        //urlParam:[]
+        getByPage: function(pageIndex) {
+            if (debug) {
+                console.log('get data for page:', pageIndex);
+            }
+            return _get({
+                host: options.host,
+                path: options.path.format(pageIndex, options.urlParam)
+            });
+        },
+        // header: function() {
+        // },
+        // refine: function(line) {
+        // };
+    }, options);
+
+    return {
+        // options: options,
+        exec: function() {
+            options.getByPage(1).then(function(data) {
+                eval(data);
+
+                // get paging info
+                var indices = [1];
+                for (var i = 2; i <= daoESData.pages; i++) {
+                    indices.push(i)
+                }
+                if (debug) {
+                    // console.log(refine(daoESData.data[0]));
+                    console.log('total page: %d', indices.length);
+                }
+
+                var result = [options.header()];
+                Promise.all(indices.map(options.getByPage)).then(function(contents) {
+                    if (debug) {
+                        console.log('done');
+                    }
+
+                    contents.forEach(function(content, index) {
+                        eval(content);
+
+                        if (daoESData && daoESData.data.length > 0) {
+                            result = result.concat(options.refine ? daoESData.data.map(options.refine) : daoESData.data);
+                            // result = result.concat(daoESData.data);
+                        } else {
+                            throw new Error('page {0}, parse failed'.format((index + 1).toString()));
+                        }
+                    });
+
+                }, function(error) {
+                    throw new Error('page {0}: get failed, error: {1}'.format(error.page, error.error));
+                }).then(function() {
+                    if (options.csvFile) {
+                        // save('./est.csv', iconv.encode(result.join('\n') + "\n", 'GB18030'));
+                        _save(options.csvFile, result.join('\n') + "\n");
+                    } else {
+                        console.dir(result);
+                        // console.log(result.join('\n') + "\n");
+                    }
+                }).catch(function(error) {
+                    throw error;
+                });
+
+            });
+        }
+    }
+};
+
+module.exports = new EM_Quote();
---
+++ nd/em2.js
@@ -0,0 +1,219 @@
+var fs = require('fs'),
+    myUtil = require('./MyUtil'),
+    anounymous = require('./ProtoUtil'),
+    em_quote = require('./EM_Quote');
+
+function EM() {
+    var downloadFA = function(code, reject, resolve) {
+        try {
+            var quote = new em_quote.Path({
+                dataType: 'DC',
+                reportType: 'SR',
+                subReportType: 'YJBB',
+                param: {
+                    'code': '{1}'
+                },
+                sort: {
+                    id: "14",
+                    desc: true
+                },
+                page: '{0}',
+                pagesize: 50
+            });
+
+            var x = new em_quote.Runner({
+                host: 'datainterface.eastmoney.com',
+                path: quote.path,
+                urlParam: [code],
+                csvFile: './cn-fa/' + code + '.csv',
+                header: function() {
+                    return "代码,名称,每股收益,每股收益(扣除),营业收入(万元),同比增长,季度环比增长,净利润(万元),同比增长,季度环比增长,每股净资产,净资产收益率,每股经营现金流,销售毛利率,利润分配,股息率,公告日期,报告期,交易所";
+                },
+            });
+
+            x.exec();
+            if (resolve) resolve(1);
+        } catch (err) {
+            if (reject) {
+                reject(err);
+            } else {
+                console.error(err);
+            }
+        }
+    };
+    var _genRecord = function(cells) {
+            return [cells[17], myUtil.toNumber(cells[2]), myUtil.toNumber(cells[10]), myUtil.toNumber(cells[11]), myUtil.toNumber(cells[12]), myUtil.toNumber(cells[13])];
+        },
+        _parseFA = function(code, reject, resolve) {
+            var srcFile = './cn-fa/' + code + '.csv',
+                dates = [
+                    '2014-12-31',
+                    '2013-12-31',
+                    '2012-12-31',
+                    '2011-12-31',
+                    '2010-12-31'
+                ];
+            fs.readFile(srcFile, function(err, data) {
+                if (err) {
+                    reject(err);
+                } else {
+                    var lines = data.toString().split(/\r\n|\n/),
+                        years = [],
+                        quarters = [];
+                    lines.forEach(function(line, index) {
+                        if (dates.length == 0) {
+                            return;
+                        }
+                        var cells = line.split(',');
+                        if (index < 5 && index > 0) {
+                            quarters.push(_genRecord(cells));
+                        }
+                        if (cells[17] == dates[0]) {
+                            years.push(_genRecord(cells));
+                            dates.shift();
+                        }
+                    });
+
+                    console.log(srcFile);
+                    console.dir(quarters);
+                    console.dir(years);
+
+                    resolve(1);
+                }
+            });
+        };
+
+    return {
+        getAnualReport: function() {
+
+            var quote = new em_quote.Path({
+                dataType: 'DC',
+                reportType: 'SR',
+                subReportType: 'YJYG',
+                param: {
+                    'fd': '{1}',
+                    'stat': '0'
+                },
+                sort: {
+                    id: "4",
+                    desc: true
+                },
+                page: '{0}',
+                pagesize: 100,
+            });
+
+            var x = new em_quote.Runner({
+                host: 'datainterface.eastmoney.com',
+                path: quote.path,
+                urlParam: ['2015-12-31'],
+                csvFile: 'es_yjyg.csv',
+                header: function() {
+                    return "代码,名称,业绩变动,业绩变动幅度,预告类型,上年同期净利润(万元),公告日期";
+                },
+                refine: function(line) {
+                    var cells = line.split(',');
+                    if (cells.length === 9) {
+                        return "{0},{1},\"{2}\",{3},{4},{5},{6}".format(cells[0], cells[1], cells[2].replace(/&sbquo/g, ','), cells[3], cells[4], cells[5], cells[7]);
+                    }
+                }
+            });
+
+            x.exec();
+        },
+        getEstimations: function() {
+
+            var quote = new em_quote.Path({
+                dataType: 'FA',
+                reportType: 'CT',
+                subReportType: 'GEMCPF',
+                param: {
+                    cmd: 'C._A',
+                    cb: '',
+                    token: '3a965a43f705cf1d9ad7e1a3e429d622',
+                    rt: '48474285'
+                },
+                sort: {
+                    id: "(Code)",
+                    desc: false
+                },
+                page: '{0}',
+                pagesize: 50,
+            });
+
+            var x = new em_quote.Runner({
+                host: 'nufm.dfcfw.com',
+                path: quote.path,
+                csvFile: 'es_ylyc.csv',
+                header: function() {
+                    return "序号,代码,名称,最新价,涨跌幅,研报数,买入,增持,中性,减持,卖出,2014收益,2015收益,2015PE,2016收益,2016PE,2017收益,2017PE,预留1,预留2,预留3";
+                },
+            });
+
+            x.exec();
+        },
+        getEarnings: function(code) {
+            downloadFA(code);
+        },
+        getEarningsBatch: function(action) {
+            var func,
+                symbolFile = '../chart/s/cn_yahoo.txt';
+
+            if (action == 'download') {
+                func = downloadFA;
+            } else if (action == 'parse') {
+                func = _parseFA;
+            } else {
+                throw new Error('don\'t support');
+            }
+
+            var fx = function(newRows, index, total, counter) {
+                if (index == total) {
+                    console.log("------------- Saved %d -------------", counter);
+                    return;
+                };
+
+                Promise.all(newRows[index].map(function(item) {
+                    return new Promise(function(resolve, reject) {
+                        if (item) {
+                            var cells = item.split(',');
+                            // console.log(cells[1]);
+
+                            func(cells[1], reject, resolve);
+
+                        } else {
+                            reject('empty');
+                        }
+                    }).catch(function(e) {
+                        console.error(e);
+                    });
+                })).then(function(val) {
+                    val.forEach(function(x) {
+                        counter += (x == undefined ? 0 : x);
+                    });
+
+                    fx(newRows, index + 1, total, counter);
+                });
+            };
+
+            var settings = {
+                    ChunkSize: 200
+                },
+                lines = myUtil.readlinesSync(symbolFile);
+
+            lines.shift();
+            lines = lines.chunk(settings.ChunkSize);
+
+            fx(lines, 0, lines.length, 0);
+        }
+    }
+};
+
+
+var util = new EM();
+
+util.getEarningsBatch('parse');
+// util.getEarnings('600545');
+// getEarnings('000635');
+
+// util.getAnualReport();
+// util.getEstimations();
