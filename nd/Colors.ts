cd28c609ded583b303e6aa910596ad0244d3ce5d
daodao10 <bebest10@gmail.com>
2016-03-09 18:12:31 +0800

    add new function rp to retrieve price from sina webservice

---
+++ nd/Colors.ts
@@ -0,0 +1,58 @@
+export class Colors {
+    static Style = {
+        "Reset": "\u001b[0m",
+        "Bright": "\u001b[1m",
+        "Dim": "\u001b[2m",
+        "Underscore": "\u001b[4m",
+        "Blink": "\u001b[5m",
+        "Reverse": "\u001b[7m",
+        "Hidden": "\u001b[8m",
+        "FG": {
+            "Black": "\u001b[30m",
+            "Red": "\u001b[31m",
+            "Green": "\u001b[32m",
+            "Yellow": "\u001b[33m",
+            "Blue": "\u001b[34m",
+            "Magenta": "\u001b[35m",
+            "Cyan": "\u001b[36m",
+            "White": "\u001b[37m"
+        },
+        "BG": {
+            "Black": "\u001b[40m",
+            "Red": "\u001b[41m",
+            "Green": "\u001b[42m",
+            "Yellow": "\u001b[43m",
+            "Blue": "\u001b[44m",
+            "Magenta": "\u001b[45m",
+            "Cyan": "\u001b[46m",
+            "White": "\u001b[47m"
+        }
+    };
+    constructor(){}
+    private inner(func, color, args):void{
+        var length = args.length;
+        if(length == 1) {
+            func(color, args[0], Colors.Style.Reset);
+        } else if(length == 2){
+            func(color, args[0], args[1], Colors.Style.Reset);
+        } else if(length == 3){
+            func(color, args[0], args[1], args[2], Colors.Style.Reset);
+        }else if (length == 4){
+            func(color, args[0], args[1], args[2], args[3], Colors.Style.Reset);
+        }else{
+            func(color, args, Colors.Style.Reset);
+        }
+    }
+    red(): void{
+        this.inner(console.log, Colors.Style.FG.Red, arguments);
+    }
+    green(): void{
+        this.inner(console.log, Colors.Style.FG.Green, arguments);
+    }
+    yellow(): void{
+        this.inner(console.log, Colors.Style.FG.Yellow, arguments);
+    }
+    blue(): void{
+        this.inner(console.log, Colors.Style.FG.Blue, arguments);
+    }
+}
---
+++ nd/rp.ts
@@ -0,0 +1,78 @@
+/// <reference path="../../node.d.ts" />
+
+'use strict';
+import {Colors} from './Colors';
+var myUtil = require('./MyUtil');
+
+class RP {
+	private out = new Colors();
+	constructor(public schedule = require('node-schedule'), private wl = require('./wl.json')) {
+		console.log('job start...');
+	}
+
+	start():void{
+		var _self = this;
+		var j = _self.schedule.scheduleJob('*/1 * * * *', ()=>{
+			// console.log(new Date(), 'The answer to life, the universe, and everything!');
+			console.log('=== ', Date.now(), ' ===');
+			Object.keys(_self.wl).forEach((market)=>{
+				_self.wl[market].forEach((s)=>{
+					_self.get(s).then((data)=>{
+						_self.out.blue(data);
+					}).catch((err)=>{
+						console.error(err);
+					});
+				});
+			});
+		});
+	}
+
+	get(symbol): Promise {
+		var _self = this;
+		var options = {
+	        	host: 'hq.sinajs.cn',
+	        	path: '/list=' + symbol,
+	        	// debug: true,
+	        	encoding: 'GBK'
+	        };
+	    return new Promise(function(resolve, reject) {
+	        myUtil.get(options, function(data, statusCode) {
+	            if (statusCode !== 200) {
+	                console.error('error occurred: ', statusCode);
+	                reject({
+	                    url: options.path,
+	                    error: statusCode
+	                });
+	            }
+	            
+	            var result = _self.parse(data);
+	            if(result) {
+	            	resolve(result);
+	            } else {
+	            	reject({
+	            		param: symbol,
+	            		error: "parse failed"
+	            	});
+	            }
+	        });
+	    });
+	}
+
+	private parse(content): Object {
+		var matches = /var hq_str_(sz|sh|hk)\d+="(.+)";/ig.exec(content);
+		if(matches && matches.length > 1) {
+			var items = matches[2].split(',');
+			return {
+				c: Number(items[4]),
+				o: Number(items[3]),
+				t: items.length > 31 ? items[31]: items[18]
+			};
+		}
+		console.log(content);
+		return null;
+	}
+}
+
+
+var rp = new RP();
+rp.start();
