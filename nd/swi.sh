d1cdfccd66728c62a5f0bfab994728222e7f2830
daodao10 <bebest10@gmail.com>
2016-05-12 12:03:49 +0800

    swi helper

---
+++ nd/swi.sh
@@ -0,0 +1,142 @@
+wget -O ./swi-hid/801001.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801001&type=510&columnid=8890"
+wget -O ./swi-hid/801002.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801002&type=510&columnid=8890"
+wget -O ./swi-hid/801003.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801003&type=510&columnid=8890"
+wget -O ./swi-hid/801005.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801005&type=510&columnid=8890"
+wget -O ./swi-hid/801010.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801010&type=510&columnid=8890"
+wget -O ./swi-hid/801020.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801020&type=510&columnid=8890"
+wget -O ./swi-hid/801030.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801030&type=510&columnid=8890"
+wget -O ./swi-hid/801040.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801040&type=510&columnid=8890"
+wget -O ./swi-hid/801050.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801050&type=510&columnid=8890"
+wget -O ./swi-hid/801060.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801060&type=510&columnid=8890"
+wget -O ./swi-hid/801070.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801070&type=510&columnid=8890"
+wget -O ./swi-hid/801080.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801080&type=510&columnid=8890"
+wget -O ./swi-hid/801090.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801090&type=510&columnid=8890"
+wget -O ./swi-hid/801100.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801100&type=510&columnid=8890"
+wget -O ./swi-hid/801110.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801110&type=510&columnid=8890"
+wget -O ./swi-hid/801120.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801120&type=510&columnid=8890"
+wget -O ./swi-hid/801130.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801130&type=510&columnid=8890"
+wget -O ./swi-hid/801140.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801140&type=510&columnid=8890"
+wget -O ./swi-hid/801150.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801150&type=510&columnid=8890"
+wget -O ./swi-hid/801160.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801160&type=510&columnid=8890"
+wget -O ./swi-hid/801170.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801170&type=510&columnid=8890"
+wget -O ./swi-hid/801180.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801180&type=510&columnid=8890"
+wget -O ./swi-hid/801190.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801190&type=510&columnid=8890"
+wget -O ./swi-hid/801200.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801200&type=510&columnid=8890"
+wget -O ./swi-hid/801210.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801210&type=510&columnid=8890"
+wget -O ./swi-hid/801220.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801220&type=510&columnid=8890"
+wget -O ./swi-hid/801230.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801230&type=510&columnid=8890"
+wget -O ./swi-hid/801250.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801250&type=510&columnid=8890"
+wget -O ./swi-hid/801260.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801260&type=510&columnid=8890"
+wget -O ./swi-hid/801270.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801270&type=510&columnid=8890"
+wget -O ./swi-hid/801280.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801280&type=510&columnid=8890"
+wget -O ./swi-hid/801300.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801300&type=510&columnid=8890"
+wget -O ./swi-hid/801710.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801710&type=510&columnid=8890"
+wget -O ./swi-hid/801720.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801720&type=510&columnid=8890"
+wget -O ./swi-hid/801730.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801730&type=510&columnid=8890"
+wget -O ./swi-hid/801740.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801740&type=510&columnid=8890"
+wget -O ./swi-hid/801750.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801750&type=510&columnid=8890"
+wget -O ./swi-hid/801760.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801760&type=510&columnid=8890"
+wget -O ./swi-hid/801770.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801770&type=510&columnid=8890"
+wget -O ./swi-hid/801780.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801780&type=510&columnid=8890"
+wget -O ./swi-hid/801790.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801790&type=510&columnid=8890"
+wget -O ./swi-hid/801811.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801811&type=510&columnid=8890"
+wget -O ./swi-hid/801812.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801812&type=510&columnid=8890"
+wget -O ./swi-hid/801813.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801813&type=510&columnid=8890"
+wget -O ./swi-hid/801821.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801821&type=510&columnid=8890"
+wget -O ./swi-hid/801822.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801822&type=510&columnid=8890"
+wget -O ./swi-hid/801823.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801823&type=510&columnid=8890"
+wget -O ./swi-hid/801831.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801831&type=510&columnid=8890"
+wget -O ./swi-hid/801832.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801832&type=510&columnid=8890"
+wget -O ./swi-hid/801833.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801833&type=510&columnid=8890"
+wget -O ./swi-hid/801841.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801841&type=510&columnid=8890"
+wget -O ./swi-hid/801842.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801842&type=510&columnid=8890"
+wget -O ./swi-hid/801843.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801843&type=510&columnid=8890"
+wget -O ./swi-hid/801851.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801851&type=510&columnid=8890"
+wget -O ./swi-hid/801852.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801852&type=510&columnid=8890"
+wget -O ./swi-hid/801853.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801853&type=510&columnid=8890"
+wget -O ./swi-hid/801861.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801861&type=510&columnid=8890"
+wget -O ./swi-hid/801862.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801862&type=510&columnid=8890"
+wget -O ./swi-hid/801863.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801863&type=510&columnid=8890"
+wget -O ./swi-hid/801880.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801880&type=510&columnid=8890"
+wget -O ./swi-hid/801890.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801890&type=510&columnid=8890"
+wget -O ./swi-hid/801901.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801901&type=510&columnid=8890"
+wget -O ./swi-hid/801903.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801903&type=510&columnid=8890"
+wget -O ./swi-hid/801905.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=801905&type=510&columnid=8890"
+wget -O ./swi-hid/802600.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=802600&type=510&columnid=8890"
+wget -O ./swi-hid/802611.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=802611&type=510&columnid=8890"
+wget -O ./swi-hid/802612.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=802612&type=510&columnid=8890"
+wget -O ./swi-hid/802613.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=802613&type=510&columnid=8890"
+wget -O ./swi-hid/803611.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=803611&type=510&columnid=8890"
+wget -O ./swi-hid/803612.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=803612&type=510&columnid=8890"
+wget -O ./swi-hid/803613.xls --referer "http://www.swsindex.com/idx0510.aspx" "http://www.swsindex.com/downloadfiles.aspx?swindexcode=803613&type=510&columnid=8890"
+python ../html2csv.py ./swi-hid/801001.xls
+python ../html2csv.py ./swi-hid/801002.xls
+python ../html2csv.py ./swi-hid/801003.xls
+python ../html2csv.py ./swi-hid/801005.xls
+python ../html2csv.py ./swi-hid/801010.xls
+python ../html2csv.py ./swi-hid/801020.xls
+python ../html2csv.py ./swi-hid/801030.xls
+python ../html2csv.py ./swi-hid/801040.xls
+python ../html2csv.py ./swi-hid/801050.xls
+python ../html2csv.py ./swi-hid/801060.xls
+python ../html2csv.py ./swi-hid/801070.xls
+python ../html2csv.py ./swi-hid/801080.xls
+python ../html2csv.py ./swi-hid/801090.xls
+python ../html2csv.py ./swi-hid/801100.xls
+python ../html2csv.py ./swi-hid/801110.xls
+python ../html2csv.py ./swi-hid/801120.xls
+python ../html2csv.py ./swi-hid/801130.xls
+python ../html2csv.py ./swi-hid/801140.xls
+python ../html2csv.py ./swi-hid/801150.xls
+python ../html2csv.py ./swi-hid/801160.xls
+python ../html2csv.py ./swi-hid/801170.xls
+python ../html2csv.py ./swi-hid/801180.xls
+python ../html2csv.py ./swi-hid/801190.xls
+python ../html2csv.py ./swi-hid/801200.xls
+python ../html2csv.py ./swi-hid/801210.xls
+python ../html2csv.py ./swi-hid/801220.xls
+python ../html2csv.py ./swi-hid/801230.xls
+python ../html2csv.py ./swi-hid/801250.xls
+python ../html2csv.py ./swi-hid/801260.xls
+python ../html2csv.py ./swi-hid/801270.xls
+python ../html2csv.py ./swi-hid/801280.xls
+python ../html2csv.py ./swi-hid/801300.xls
+python ../html2csv.py ./swi-hid/801710.xls
+python ../html2csv.py ./swi-hid/801720.xls
+python ../html2csv.py ./swi-hid/801730.xls
+python ../html2csv.py ./swi-hid/801740.xls
+python ../html2csv.py ./swi-hid/801750.xls
+python ../html2csv.py ./swi-hid/801760.xls
+python ../html2csv.py ./swi-hid/801770.xls
+python ../html2csv.py ./swi-hid/801780.xls
+python ../html2csv.py ./swi-hid/801790.xls
+python ../html2csv.py ./swi-hid/801811.xls
+python ../html2csv.py ./swi-hid/801812.xls
+python ../html2csv.py ./swi-hid/801813.xls
+python ../html2csv.py ./swi-hid/801821.xls
+python ../html2csv.py ./swi-hid/801822.xls
+python ../html2csv.py ./swi-hid/801823.xls
+python ../html2csv.py ./swi-hid/801831.xls
+python ../html2csv.py ./swi-hid/801832.xls
+python ../html2csv.py ./swi-hid/801833.xls
+python ../html2csv.py ./swi-hid/801841.xls
+python ../html2csv.py ./swi-hid/801842.xls
+python ../html2csv.py ./swi-hid/801843.xls
+python ../html2csv.py ./swi-hid/801851.xls
+python ../html2csv.py ./swi-hid/801852.xls
+python ../html2csv.py ./swi-hid/801853.xls
+python ../html2csv.py ./swi-hid/801861.xls
+python ../html2csv.py ./swi-hid/801862.xls
+python ../html2csv.py ./swi-hid/801863.xls
+python ../html2csv.py ./swi-hid/801880.xls
+python ../html2csv.py ./swi-hid/801890.xls
+python ../html2csv.py ./swi-hid/801901.xls
+python ../html2csv.py ./swi-hid/801903.xls
+python ../html2csv.py ./swi-hid/801905.xls
+python ../html2csv.py ./swi-hid/802600.xls
+python ../html2csv.py ./swi-hid/802611.xls
+python ../html2csv.py ./swi-hid/802612.xls
+python ../html2csv.py ./swi-hid/802613.xls
+python ../html2csv.py ./swi-hid/803611.xls
+python ../html2csv.py ./swi-hid/803612.xls
+python ../html2csv.py ./swi-hid/803613.xls
