3b1dff6e7f1bc47754b28260dc2acb349bc879bb
daodao10 <bebest10@gmail.com>
2017-01-30 06:32:19 +0800

    fixed bug

--- nd/yahoo_etl.js
+++ nd/yahoo_etl.js
@@ -33,37 +33,47 @@ function save(csvFile, data) {
 }
 
 function updateLastPrice(counter) {
-    var options = {
-        host: 'download.finance.yahoo.com',
-        path: '/d/quotes.csv?s={0}&f=d1l1'.format(counter[0])
-    };
+    var
+        options = {
+            host: 'download.finance.yahoo.com',
+            path: '/d/quotes.csv?s={0}&f=d1l1'.format(counter[0])
+        },
+        mock = false;
 
     get(options).then(function (data) {
         // console.log(data);
         // "11/10/2016",2167.48
         if (data) {
-            var arr = data.stripLineBreaks().split(',');
-            var doc = {
-                "_id": arr[0].substr(7, 4) + arr[0].substr(1, 2) + arr[0].substr(4, 2),
-                "c": Number(arr[1])
-            };
+            var
+                arr = data.stripLineBreaks().split(','),
+                dateArr = arr[0].replace(/"/g, '').split('/'),
+                dt = new Date(myUtil.toNumber(dateArr[2]), myUtil.toNumber(dateArr[0]) - 1, myUtil.toNumber(dateArr[1])),
+                doc = {
+                    "_id": dt.format('yyyyMMdd'),
+                    "c": Number(arr[1])
+                };
 
-            var myMongo = new MyMongo("{0}{1}".format(config.DbSettings.DbUri, 'test'));
-            myMongo.insert(counter[1], doc, function (err, insertResult) {
-                if (err) {
-                    console.error(err);
-                } else {
-                    console.log(insertResult.ops);
-                }
-            });
+            if (mock) {
+                console.log(data);
+                console.log(doc);
+            } else {
+                var myMongo = new MyMongo("{0}{1}".format(config.DbSettings.DbUri, 'test'));
+                myMongo.insert(counter[1], doc, function (err, insertResult) {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.log(insertResult.ops);
+                    }
+                });
+            }
         }
     });
 }
 
 [
     ['^GSPC', 'spx'],
-    // ['^IXIC', 'ndq'],
-    // ['^RUT', 'rut']
+    ['^IXIC', 'ndq'],
+    ['^RUT', 'rut']
 ].forEach(function (item) {
     updateLastPrice(item);
 });
