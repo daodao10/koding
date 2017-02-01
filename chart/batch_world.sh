5a1d356c1fa09021fb5d76aee2d3835fbd65a5ea
daodao10 <bebest10@gmail.com>
2017-02-01 19:16:51 +0800

    update symbol files

--- chart/batch_world.sh
+++ chart/batch_world.sh
@@ -4,13 +4,13 @@ node opt_pre_etl.js s/future.txt 1 >> dl_world.sh
 node opt_pre_etl.js s/stooq.txt 1 >> dl_world.sh
 # download
 sh dl_world.sh
-python google_etl.py s/google.txt
+python google_etl.py s/google_d.txt
 # etl opt
 node opt_etl.js s/indices.txt
 node opt_etl.js s/future.txt
 node opt_etl.js s/stooq.txt
-node opt_etl.js s/google.txt
-node opt_etl.js s/dao_d.txt
+node opt_etl.js s/google_d.txt
+node opt_etl.js s/yahoo_d.txt
 
 cd ../nd
 # BDI etl
