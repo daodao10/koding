751b8a662950973e9fa63d352e18e2e5be6b6e2c
daodao10 <bebest10@gmail.com>
2017-11-13 21:20:47 +0800

    easymoney f10 changed

--- nd/em-f10_etl.ts
+++ nd/em-f10_etl.ts
@@ -1,6 +1,8 @@
 /// <reference path="../../node.d.ts" />
 // etl for f10.eastmoney.com
-// http://f10.eastmoney.com/f10_v2/CapitalStockStructure.aspx?code=sh600298
+// api has been changed
+// http://emweb.securities.eastmoney.com/f10_v2/CapitalStockStructure.aspx?type=web&code=sh600298
+// http://emweb.securities.eastmoney.com/PC_HSF10/CapitalStockStructure/CapitalStockStructureAjax?code=sh600298
 
 "use strict";
 
@@ -22,7 +24,8 @@ class EM3 {
 
     private _request(options: Object): Promise {
         options = myUtil.extend({
-            host: 'f10.eastmoney.com'
+            // host: 'f10.eastmoney.com'
+            host: 'emweb.securities.eastmoney.com'
         }, options);
         return new Promise(function (resolve, reject) {
             myUtil.request(options, function (data, statusCode) {
@@ -72,34 +75,43 @@ class EM3 {
 
     capital_etl(code: String) {
         var options = {
-            path: '/f10_v2/CapitalStockStructure.aspx?code=' + code,
+            // path: '/f10_v2/CapitalStockStructure.aspx?type=web&code=' + code,
+            path: '/PC_HSF10/CapitalStockStructure/CapitalStockStructureAjax?code=' + code,
+            charset: 'UTF-8',
             // debug: true
         };
         this._request(options).then((content: String) => {
             // console.log(content);
             var result = { _id: code };
 
-            this._regExs.forEach((reg, index) => {
-                var x = this._parse(content, reg);
-                if (x) {
-                    if (index == 0) result.mv = myUtil.toNumber(x[0]);
-                    else result.nv = myUtil.toNumber(x[1]);
-                }
-            });
+            // this._regExs.forEach((reg, index) => {
+            //     var x = this._parse(content, reg);
+            //     if (x) {
+            //         if (index == 0) result.mv = myUtil.toNumber(x[0]);
+            //         else result.nv = myUtil.toNumber(x[1]);
+            //     }
+            // });
+
+            var x = JSON.parse(content);
+            var shares = x.Result.UnlistedShareChangeList;
+            result.mv = myUtil.toNumber(shares[1].changeList[0]);
+            result.nv = myUtil.toNumber(shares[2].changeList[0]);
 
             // console.log(result);
             this._myMongo.upsertBatch('counterCN', [result], function (err, result) {
                 _this._logDbResult('update mv/nv to ' + code, err, result);
             });
+        }, (err) => {
+            console.log(err);
         });
     }
 }
 
 var em3 = new EM3([/<th class="tips-fieldnameL">股份总数<\/th><td class="tips-dataR">([0-9,.]+)<\/td>/ig,
     /<tr><th class="tips-fieldnameL">已上市流通A股<\/th><td class="tips-dataR">([0-9,.]+)<\/td>/ig]);
-// // 1)
+// // 1) single counter
 // em3.capital_etl('sz002669');
-// // 2)
+// // 2) array of counter
 // [
 //     'SZ300575',
 //     'SZ300576',
@@ -107,24 +119,30 @@ var em3 = new EM3([/<th class="tips-fieldnameL">股份总数<\/th><td class="tip
 // ].forEach((code) => {
 //     em3.capital_etl(code);
 // });
-// // 3)
-// (function (lines) {
-//     lines.forEach((item, index) => {
-//         // if (index > 500) return;
+// 3) symbol file
+(function (lines) {
+    lines.forEach((item, index) => {
+        // if (index > 500) return;
 
-//         // if (index < 500) return;
-//         // if (index > 1000) return;
-//         // if (index < 1000) return;
-//         // if (index > 1500) return;
-//         // if (index < 2000) return;
-//         // if (index > 2500) return;
-//         // if (index < 2500) return;
-//         // if (index > 3000) return;
+        // if (index < 500) return;
+        // if (index > 1000) return;
 
-//         // if (index < 3000) return;
-//         if (item) {
-//             item = (item.startsWith('6') ? 'SH' : 'SZ') + item;
-//             em3.capital_etl(item);
-//         }
-//     });
-// } (myUtil.readlinesSync('./-hid/symbol.txt')));
+        // if (index < 1000) return;
+        // if (index > 1500) return;
+
+        // if (index < 1500) return;
+        // if (index > 2000) return;
+
+        // if (index < 2000) return;
+        // if (index > 2500) return;
+
+        // if (index < 2500) return;
+        // if (index > 3000) return;
+
+        if (index < 3000) return;
+        if (item) {
+            item = (item.startsWith('6') ? 'SH' : 'SZ') + item;
+            em3.capital_etl(item);
+        }
+    });
+} (myUtil.readlinesSync('./-hid/cn_symbol.txt')));
