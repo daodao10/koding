7b85be8591491cbfb09b0e6d64982b3875d5163d
daodao10 <bebest10@gmail.com>
2019-03-06 23:19:51 +0800

    add calculator of pyramid

---
+++ nd/PyramidCalc.ts
@@ -0,0 +1,76 @@
+import * as util from "util";
+
+/**
+底边长 bottom length - L
+斜面高 slope height - S
+塔高   height - h
+斜边长 hypotenuse length - hl
+=>
+2*L/h = math.PI
+L*S /2 = h^2
+S/(L/2) = 1.618
+ */
+
+class Pyramid {
+  bottom: number;
+  slopHeight: number;
+  height: number;
+  hypotenuse: number;
+  angle: number;
+  angle2: number;
+
+  /**
+   *
+   */
+  constructor(width: number) {
+    this.bottom = width;
+  }
+
+  calc(): void {
+    // 2*L/h = math.PI
+    this.height = (2 * this.bottom) / Math.PI;
+
+    // L*S /2 = h^2
+    // this.slopHeight = (this.height ** 2 * 2) / this.bottom;
+    // S/(L/2) = 1.618
+    this.slopHeight = 1.618 * (this.bottom / 2);
+    // console.log(this.slopHeight)
+
+    // according to The Pythagorean theorem
+    // hl^2 = S^2 + (L/2)^2
+    this.hypotenuse = Math.sqrt(this.slopHeight ** 2 + (this.bottom / 2) ** 2);
+
+    this.angle = this.calcAngle(this.height / this.slopHeight);
+    this.angle2 = this.calcAngle(this.slopHeight / this.hypotenuse);
+  }
+
+  calcAngle(sinValue: number) {
+    /**
+     * double v = sin(0.3)
+     * double v1= asin(v)
+     * double v2 = v1/2/PI*360
+     */
+    return (Math.asin(sinValue) / 2 / Math.PI) * 360;
+  }
+
+  toString() {
+    return util.format(
+      "bottom: %d, heigth: %d, hypotenuse: %d, \nslope-height: %d, angle: %d, angle2: %d",
+      this.bottom.toFixed(2),
+      this.height.toFixed(2),
+      this.hypotenuse.toFixed(2),
+      this.slopHeight.toFixed(2),
+      this.angle,
+      this.angle2
+    );
+  }
+}
+
+if (process.argv.length === 3) {
+  let width = parseInt(process.argv[2], 10);
+  let pyr = new Pyramid(width);
+  pyr.calc();
+  console.log(pyr.toString());
+} else {
+  console.log("please follow bottom-width of your Pyramid to command");
+}
