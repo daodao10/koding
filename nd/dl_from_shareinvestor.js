064c6b7b4be60844f274f6f95ffc8fe33b957c59
daodao10 <bebest10@gmail.com>
2019-03-31 15:46:55 +0800

    remove dependency, support automatically download

--- nd/dl_from_shareinvestor.js
+++ nd/dl_from_shareinvestor.js
@@ -1,6 +1,16 @@
-const puppeteer = require("puppeteer"),
-  CREDS = require("./creds"),
-  myUtil = require("./MyUtil");
+const fs = require("fs"),
+  util = require("util"),
+  puppeteer = require("puppeteer"),
+  CREDS = require("./creds");
+
+// const { TimeoutError } = require("puppeteer/Errors");
+process
+  .on("uncaughtException", function(err) {
+    console.error("Caught exception: " + err);
+  })
+  .on("unhandledRejection", err => {
+    console.error("Caught exception: " + err);
+  });
 
 const USERNAME_SELECTOR = "#sic_login_header_username";
 const PASSWORD_SELECTOR = "#sic_login_header_password";
@@ -18,6 +28,11 @@ const setDownloadBehavior = (
   });
 };
 
+async function delay(ms) {
+  // return await for better async stack trace support in case of errors.
+  return await new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function run(symbolList, startIndex) {
   const browser = await puppeteer.launch({
     headless: false,
@@ -26,11 +41,11 @@ async function run(symbolList, startIndex) {
   });
 
   const page = await browser.newPage();
-
   await page.goto("https://www.shareinvestor.com/user/login.html");
-  // await page.waitForNavigation();
-  // await page.screenshot({ path: "screenshots/shareinvestor.png" })
+
   setDownloadBehavior(page);
+
+  await page.waitForSelector(BUTTON_SELECTOR);
 
   await page.focus(USERNAME_SELECTOR);
   await page.keyboard.type(CREDS.username);
@@ -39,61 +54,75 @@ async function run(symbolList, startIndex) {
   await page.keyboard.type(CREDS.password);
 
   await page.click(BUTTON_SELECTOR);
+
   await page.waitForNavigation();
 
   let batchCount = 30;
-  let prompt1 = "current index: %d, please press [go] to continue ...";
-  let prompt2 = "try to process next %d counters";
+  let prompt1 = "[%s] current index: %d, try to process next %d counters";
+  let prompt2 =
+    "[%s] sleep 21 minutes then keep working again, press [ctrl + c] to exit ...";
 
-  console.log(prompt1, startIndex);
-  process.stdin.on("data", async function(key) {
-    // ctrl-c ( end of text )
-    if (key === "\u0003") {
-      process.exit();
+  while (startIndex < symbolList.length) {
+    console.log(
+      prompt1,
+      new Date().toLocaleTimeString(),
+      startIndex,
+      batchCount
+    );
+
+    let start = startIndex;
+    let counters = symbolList.slice(startIndex, startIndex + batchCount);
+    for (let counter of counters) {
+      await download(page, counter);
+      startIndex++;
     }
 
-    if (key.indexOf("go") == 0) {
-      console.log(prompt2, batchCount);
-      let start = startIndex;
-      let counters = symbolList.slice(startIndex, startIndex + batchCount);
-      for (let counter of counters) {
-        await download(page, counter);
-        startIndex++;
-      }
-      if (startIndex === start + batchCount) {
-        console.log(prompt1, startIndex);
-      }
+    if (startIndex === start + batchCount) {
+      console.log(prompt2, new Date().toLocaleTimeString());
+      await delay(21 * 60 * 1000);
+      console.log("...");
     }
-  });
+  }
 
-  //   browser.close()
+  console.log("all done");
+  browser.close();
 }
 
 async function download(page, counter) {
-  console.log("process ", counter);
-
   await page.goto(
     `https://www.shareinvestor.com/prices/price_download.html#/?type=price_download_by_stock&counter=${counter}`
   );
-  await page.waitFor(5 * 1000);
+  await page.waitForSelector(DOWNLOAD_BUTTON_SELECTOR);
+  await page.waitFor(3 * 1000);
   await page.click(DOWNLOAD_BUTTON_SELECTOR);
-  // await page.waitForNavigation();
+  await page.waitFor(5 * 1000);
+  console.log(`${counter} => done`);
 }
 
-const getAllSymbols = () => {
-  let lines = myUtil.readlinesSync("../chart/s/sg_shareinvestor.txt");
+async function getSymbols(filepath) {
+  const readFile = util.promisify(fs.readFile);
+
   let x = [],
     cells;
-  lines.forEach((line, index) => {
-    if (index === 0) return;
-    cells = line.split(",");
-    if (cells.length > 1) x.push(cells[0]);
-  });
+
+  try {
+    let content = await readFile(filepath);
+    let lines = content.toString().split(/\r\n|\n/);
+    lines.forEach((line, index) => {
+      if (index === 0) return;
+      cells = line.split(",");
+      if (cells.length > 1) x.push(cells[0]);
+    });
+  } catch (err) {
+    console.log("Error", err);
+  }
+
   return x;
-};
+}
 
-let allSymbols = getAllSymbols();
-let patchSymbols = [];
-
-// run(getAllSymbols(), 23);
-run(patchSymbols, 0);
+getSymbols("../chart/s/sg_shareinvestor.txt")
+  .then(symbols => {
+    console.log('all symbol:', symbols.length);
+    run(symbols, 22);
+  })
+  .catch(err => console.error(err));
